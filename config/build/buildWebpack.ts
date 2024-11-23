03b14d0d1100e42ea41c7866a313dbf643d96441
Emil Mustafin <emill.mustafin@yandex.ru>
2024-11-23 19:20:06 +0300

    fix:update deploy

--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -15,7 +15,7 @@ export function buildWebpack(options: BuildOptions): Configuration {
       path: paths.output,
       filename: 'js/[name].[contenthash].js',
       clean: true,
-      publicPath: '',
+      publicPath: isDev ? '/' : '/historical-dates/',
     },
     plugins: buildPlugins(options),
     module: {
