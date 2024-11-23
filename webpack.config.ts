1768384c729c0b950bbc58f25fe8e2d68d1c07a5
Emil Mustafin <emill.mustafin@yandex.ru>
2024-11-23 20:03:41 +0300

    fix:update

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,8 +11,8 @@ export default (env: EnvVariables) => {
   const paths: BuildPaths = {
     output: path.resolve(__dirname, 'build'),
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    html: path.resolve(__dirname, 'docs', 'index.html'),
-    public: path.resolve(__dirname, 'docs'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    public: path.resolve(__dirname, 'public'),
     src: path.resolve(__dirname, 'src'),
   };
 
